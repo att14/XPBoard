164b6ae8620595ec00e5c89b1e97412c20652ef8
Ivan Malison <IvanMalison@gmail.com>
2013-07-14 23:47:14 -0700

    Only show pending reviews.

--- xp_board/routes.py
+++ xp_board/routes.py
@@ -8,7 +8,7 @@ from . import config
 def review_board_dashboard():
     users = sorted(
         models.User.list_by_column_values(config.users, 'username'),
-        key=lambda user: -len(user.primary_reviews)
+        key=lambda user: -len(user.pending_primary_reviews)
     )
     return render_template(
         'reviewboard_dashboard.html',
