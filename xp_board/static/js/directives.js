28b4e3b2d745b639c8bfc4e6b82575c7f307c819
Ivan Malison <IvanMalison@gmail.com>
2013-07-25 13:21:01 -0700

    Added some templates.

---
+++ xp_board/static/js/directives.js
@@ -0,0 +1,21 @@
+angular.module('XPBoard').directive(
+  'stickyColumn', function() {
+    return {
+      replace: True,
+      scope: {
+        tickets: "=tickets"
+      },
+      templateUrl: 
+    }
+  }
+).directive(
+  'sticky', function() {
+    return {
+      replace: True,
+      scope: {
+        
+      }
+    }
+  }
+);
+                                    
